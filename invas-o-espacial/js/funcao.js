1cc9459ba74b2bb7aaa92f2b86b41abd52143412
FelipeHintz <felipe.hintz@catolicasc.org.br>
2017-03-14 15:02:04 -0300

    Atualização da Sprite do tiro e pequenas correções no código.

--- invas-o-espacial/js/funcao.js
+++ invas-o-espacial/js/funcao.js
@@ -2,7 +2,10 @@ var imgjogo = new Image;
 var imgPersonagem = new Image;
 var imgTiro = new Image;
 
-var personagem = {
+var cont = 1;
+var fogo;
+
+var jogador = {
 	"x":260,
 	"y":570,
 	"largura":80,
@@ -19,14 +22,14 @@ var personagem = {
 var tiro = {
 	"x": 260,
 	"y": 570,
-	"largura": 67,
-	"altura": 67,
-	"direcaoAtual":0,
-	"direcao":{
-			"frente": 0,
-			"esquerda": 105,
-			"direita": 210,
-			"atras": 315
+	"largura": 35,
+	"altura": 70,
+	"faseAtual":-70,
+	"fase":{
+			"fase1": 0,
+			"fase2": 70,
+			"fase3": 140,
+			"fase4": 210
 	}
 }
 
@@ -40,42 +43,63 @@ imgTiro.src = 'img/tiro.png';
 	canvas.height= 700;
 	canvas.width= 600;
 
-	ctx.drawImage( imgPersonagem, 0,personagem.direcao.esquerda,personagem.largura,personagem.altura,personagem.x,personagem.y,personagem.largura,personagem.altura);
-	ctx.drawImage( imgTiro, 0,tiro.direcao.frente,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
+	ctx.drawImage( imgPersonagem, 0,jogador.direcao.esquerda,jogador.largura,jogador.altura,jogador.x,jogador.y,jogador.largura,jogador.altura);
+	ctx.drawImage( imgTiro, 0,tiro.fase.fase0,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
 	
 	setInterval(function(){
 		ctx.clearRect(0,0,canvas.width,canvas.height);
-		ctx.drawImage( imgPersonagem, 0,personagem.direcaoAtual,personagem.largura,personagem.altura,personagem.x,personagem.y,personagem.largura,personagem.altura);
-		ctx.drawImage( imgTiro, 0,tiro.direcao.frente,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
+		ctx.drawImage( imgPersonagem, 0,jogador.direcaoAtual,jogador.largura,jogador.altura,jogador.x,jogador.y,jogador.largura,jogador.altura);
+		ctx.drawImage( imgTiro, 0,tiro.fase.fase0,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
 	});
 
 	window.addEventListener("keydown", function keydown(e) {
 		var keycode = e.which || window.event.keycode;
 		switch (keycode) {
 			case 37:
-				if (personagem.x > 20){
-					personagem.direcaoAtual=personagem.direcao.esquerda;
-					personagem.x-=30;
+				if (jogador.x > 20){
+					jogador.direcaoAtual=jogador.direcao.esquerda;
+					jogador.x-=30;
 				}
 				break;
 			case 39:
-				if (personagem.x < 520){
-					personagem.direcaoAtual=personagem.direcao.direita;
-					personagem.x+=30;
+				if (jogador.x < 520){
+					jogador.direcaoAtual=jogador.direcao.direita;
+					jogador.x+=30;
 				}
 				break;
 				
 			case 32:
-				setInterval(movTiro,30);
+				fogo = setInterval(movTiro,30);
 				break;
 		}
 	});
 	window.addEventListener("keyup", function keydown(e) {
-		personagem.direcaoAtual=personagem.direcao.frente;
+		jogador.direcaoAtual=jogador.direcao.frente;
 	});
 	
 	function movTiro(){
 		tiro.y -= 30;
+		if (cont == 1){
+			ctx.drawImage( imgTiro, 0,tiro.fase.fase1,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
+			console.log(cont);
+			cont++;
+		} else if (cont == 2){
+			ctx.drawImage( imgTiro, 0,tiro.fase.fase2,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
+			console.log(cont);
+			cont++;
+		} else if (cont == 3){
+			ctx.drawImage( imgTiro, 0,tiro.fase.fase3,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
+			console.log(cont);
+			cont++;
+		} else {
+			ctx.drawImage( imgTiro, 0,tiro.fase.fase4,tiro.largura,tiro.altura,tiro.x,tiro.y,tiro.largura,tiro.altura);
+			console.log(cont);
+			cont = 1;
+		}
+		if (tiro.y < -30){
+			clearInterval(fogo);
+			tiro.y = 570;
+		}
 	}
 
 //};
